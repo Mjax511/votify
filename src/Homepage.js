0fbc573898b3223005e447c2bf74490a5afd389e
Matthew Jackson <mjax@Matthews-MacBook-Air.local>
2020-10-21 09:59:32 -0700

    fetching with accessToken in Homepage component

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 export const Homepage = () => {
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${sessionStorage.accessToken}`);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("https://api.spotify.com/v1/me", requestOptions)
+      .then(response => response.json())
+      .then(res => {
+        sessionStorage.setItem("name", res.display_name)
+      })
+      .catch(error => console.log('error', error));
+
     return (
-        <div>Homepage</div>
+        <div>{sessionStorage.name}</div>
     );
 };
