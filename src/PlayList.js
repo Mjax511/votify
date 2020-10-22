88694d9e70e290558493ea832977668bc216e953
mjax <matt.d.jax@gmail.com>
2020-10-21 20:58:47 -0700

    handled state and loading

--- src/PlayList.js
+++ src/PlayList.js
@@ -1,38 +1,41 @@
 import React from "react";
 
 export const PlayList = () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${sessionStorage.accessToken}`);
+  const [loading, setLoading] = React.useState(false);
+  const [playlist, setPlaylist] = React.useState(null);
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${sessionStorage.accessToken}`);
 
-    fetch("https://api.spotify.com/v1/me/playlists", requestOptions)
-        .then(response => response.json())
-        .then(res => {
-            sessionStorage.setItem("playlists", JSON.stringify(res.items))
-        })
-        .catch(error => console.log('error', error));
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-    const listPlay = (list) => {
-        //console.log(sessionStorage.playlists.length)
-        if (list !== undefined) {
-            var parcedList = JSON.parse(list)
-            // for (var i = 0; i < parcedList.length; i++) {
-                // console.log(parcedList[i].name)
-                return Object.keys(parcedList).map(key => <li key={key}>{parcedList[key].name}</li>)
-    
-            // }
-            // console.log(parcedList)
-            // console.log(Object.keys(parcedList))
-            // console.log(Object.keys(parcedList).map(key => <li>{parcedList[Number(key)]}</li>))
-        }
-
+  const listPlay = (list) => {
+    if (list !== undefined) {
+      return Object.keys(list).map((key) => (
+        <li key={key}>{list[key].name}</li>
+      ));
     }
+  };
 
-    return <ul>{listPlay(sessionStorage.playlists)}</ul>
+  if (playlist) {
+    return <ul>{listPlay(playlist)}</ul>;
+  }
+  if (loading) {
+    return <div>Playlist Loading</div>;
+  }
 
-}
+  setLoading(true);
+  fetch("https://api.spotify.com/v1/me/playlists", requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      setPlaylist(res.items);
+      setLoading(false);
+    })
+    .catch((error) => console.log("error", error));
+
+  return <div>Playlist Loading</div>;
+};
