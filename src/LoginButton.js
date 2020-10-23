f5a31049e8028850ea9422b04b5f0be06659d7b8
mjax <matt.d.jax@gmail.com>
2020-10-22 23:13:40 -0700

    cleanup

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,19 +1,21 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
+import { Button } from "@material-ui/core";
+import React from "react";
 
 export const LoginButton = () => {
-   
-    const redirect_uri = 'http://localhost:3000'
-    const my_client_id = 'd61d9c2cce0241c1bee240e797303b23'
-    const scopes = 'playlist-read-collaborative playlist-modify-public'
-    //const authHeader = 'Basic ZDYxZDljMmNjZTAyNDFjMWJlZTI0MGU3OTczMDNiMjM6ZmFlMzFiNWI5NTIwNDQ2Mzk0OTE1NzJkYWJkNDNlN2Q='
-    const spotifyAuthURI = `https://accounts.spotify.com/authorize?response_type=code&client_id=${my_client_id}${scopes ? '&scope=' + encodeURIComponent(scopes) : ''}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
-    const onClick = () => {
-        window.location = spotifyAuthURI
-    }
+  const redirect_uri = "http://localhost:3000";
+  const my_client_id = "d61d9c2cce0241c1bee240e797303b23";
+  const scopes = "playlist-read-collaborative playlist-modify-public";
+  //const authHeader = 'Basic ZDYxZDljMmNjZTAyNDFjMWJlZTI0MGU3OTczMDNiMjM6ZmFlMzFiNWI5NTIwNDQ2Mzk0OTE1NzJkYWJkNDNlN2Q='
+  const spotifyAuthURI = `https://accounts.spotify.com/authorize?response_type=code&client_id=${my_client_id}${
+    scopes ? "&scope=" + encodeURIComponent(scopes) : ""
+  }&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  const onClick = () => {
+    window.location = spotifyAuthURI;
+  };
 
-    return (
-        <Button variant='contained' color='primary' onClick={onClick}>Login</Button>
-    )
-
-}
+  return (
+    <Button variant="contained" color="primary" onClick={onClick}>
+      Login
+    </Button>
+  );
+};
