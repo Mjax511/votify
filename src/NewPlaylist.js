c591c5b6292f8ca69c13d9293cc8447cbafb4dd2
mjax <matt.d.jax@gmail.com>
2020-10-31 19:05:35 -0700

    can now add playlist to users account with input form

--- src/NewPlaylist.js
+++ src/NewPlaylist.js
@@ -9,11 +9,11 @@ export const NewPlaylist = () => {
   myHeaders.append("Authorization", `Bearer ${sessionStorage.accessToken}`);
   myHeaders.append("Content-Type", "application/json");
 
-  var raw = {
-    name: { input },
+  var raw = JSON.stringify({
+    name: input,
     description: "Votify Playlist",
     public: true,
-  };
+  });
 
   var requestOptions = {
     method: "POST",
@@ -22,12 +22,14 @@ export const NewPlaylist = () => {
     redirect: "follow",
   };
 
-  const handleEnter = (e) => {
+  const handleEnter = async (e) => {
     if (e.key === "Enter") {
-      fetch("https://api.spotify.com/v1/users/mjaxs/playlists", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.log("error", error));
+      // await fetch("https://api.spotify.com/v1/users/mjaxs/playlists", requestOptions)
+      //   .then((response) => response.text())
+      //   .then((result) => console.log(result))
+      //   .catch((error) => console.log("error", error));
+
+        e.target.value = "";
     }
   };
   const handleChange = (e) => {
